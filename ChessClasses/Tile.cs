c5e5444a0eeca69335458568fe26ef50e19c897c
Daniel Boyko <56567623+DanBoyko@users.noreply.github.com>
2022-12-27 16:48:14 -0700

    Update Tile.cs

--- ChessClasses/Tile.cs
+++ ChessClasses/Tile.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+sing ChessClasses;
+
 namespace ChessClasses
 {
     public class Tile
     {
+        Piece? piece;
+        int xcoord;
+        int ycoord;
+
+        public Tile(int x, int y, Piece? piece)
+        {
+            this.xcoord = x;
+            this.ycoord = y;
+            this.piece = piece;
+        }
+
+        public void RemovePiece(Piece piece)
+        {
+            this.piece = null;
+        }
+
+        public void AddPiece(Piece piece)
+        {
+            this.piece = piece;
+        }
+
+        public bool IsBlack(int xcoord, int ycoord)
+        {
+            return ((xcoord + ycoord) % 2) == 0;
+        }
     }
 }
